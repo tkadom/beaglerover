fcfde9dca772c0c052bec88e433ea817fff922b0
tkadom <tkadom@gmail.com>
2014-02-11 19:12:49 +0000

    adds restful motor

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var restify = require('restify');
+var motorSave = require('save')('motor');
 
 var ip_addr = '127.0.0.1';
 var port = 9090 ;
@@ -10,5 +11,11 @@ name : "beaglerover"
 server.listen(port, ip_addr, function(){
 		console.log('%s listening at %s ', server.name, server.url);
 });
+
+server.get('/motor', function (req, res, next) {
+	motorSave.find({}, function (Error, motors) {
+		res.send(motors)
+	})
+})
 
 
