6a31b3d41574f54daf2706869c62ddc7aed05dc0
root <root@beagleblue.(none)>
2014-02-16 21:58:37 +0000

    initial robot routes

--- roverserver/app.js
+++ roverserver/app.js
@@ -11,6 +11,14 @@ var path = require('path');
 
 var app = express();
 
+// initialize beagle bone
+var bscript=require('bonescript');
+bscript.pinMode('P8_13', bscript.OUTPUT); // Servo
+bscript.pinMode('P9_14', b.OUTPUT); // left motor 
+bscript.pinMode('P9_16', b.OUTPUT);
+bscript.pinMode('P9_21', b.OUTPUT); // right motor 
+bscript.pinMode('P9_22', b.OUTPUT);
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -32,10 +40,33 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 
 app.get('/forward', function (req, res) {
-		  res.send('Moving Forward');
+     bscript.analogWrite('P8_13', 0.01 , 60);
+  res.send('Moving Forward now');
 });
+
+app.get('/backward', function (req, res) {
+     bscript.analogWrite('P8_13', 0.01 , 60);
+  res.send('Moving Forward now');
+});
+
+app.get('/left', function (req, res) {
+     bscript.analogWrite('P8_13', 0.01 , 60);
+  res.send('Moving Forward now');
+});
+
+app.get('/right', function (req, res) {
+     bscript.analogWrite('P8_13', 0.01 , 60);
+  res.send('Moving Forward now');
+});
+
 app.get('/stop', function (req, res) {
-		  res.send('stopping');
+	bscript.analogWrite('P8_13', 0.09 , 60);
+	res.send('stopping now');
+});
+
+app.get('/angle', function (req, res) {
+	bscript.analogWrite('P8_13', 0.09 , 60);
+	res.send('stopping now');
 });
 
 http.createServer(app).listen(app.get('port'), function(){
